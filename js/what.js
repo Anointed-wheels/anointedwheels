54695eda813afbd389246c4ebd241aa72edbcbda
Anointed-wheels <toluleyeoluwaseyi@gmail.com>
2026-03-11 09:40:59 +0100

    push

--- js/what.js
+++ js/what.js
@@ -50,3 +50,46 @@ window.location.href = mail;
 }
 
 });
+
+const cards = document.querySelectorAll(".project-card");
+
+const modal = document.getElementById("projectModal");
+
+const modalImg = document.getElementById("modalImg");
+
+const modalDesc = document.getElementById("modalDesc");
+
+const closeModal = document.querySelector(".close-modal");
+
+
+cards.forEach(card=>{
+
+card.addEventListener("click",()=>{
+
+modal.style.display="flex";
+
+modalImg.src = card.dataset.img;
+
+modalDesc.textContent = card.dataset.desc;
+
+});
+
+});
+
+
+closeModal.addEventListener("click",()=>{
+
+modal.style.display="none";
+
+});
+
+
+modal.addEventListener("click",(e)=>{
+
+if(e.target === modal){
+
+modal.style.display="none";
+
+}
+
+});
